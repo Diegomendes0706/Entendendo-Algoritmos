9c2cf5ab05489f4ac4937a8a1e2f6ae671347de7
Diegomendes0706 <diego.mendesgarcia@gmail.com>
2023-11-26 15:30:52 -0300

    verificadas lista

--- pesquisaemlargura1.py
+++ pesquisaemlargura1.py
@@ -1,31 +1,35 @@
-    from collections import deque
+from collections import deque
 
 
-    def pessoa_e_vendedor(nome):
-        return nome[0] == 'B' or nome[0] == 'F'
+def pessoa_e_vendedor(nome):
+    return nome[0] == 'B' or nome[0] == 'F'
 
 
-    def busca_mango_vendedor(grafo, nome):
-        fila = deque()
-        fila += grafo[nome]
-        while fila:
-            pessoa = fila.popleft()
+def busca_mango_vendedor(grafo, nome):
+    fila = deque()
+    fila += grafo[nome]
+    verificadas = []
+    while fila:
+        pessoa = fila.popleft()
+        if not pessoa in verificadas:
             if pessoa_e_vendedor(pessoa):
                 print(pessoa + " é vendedor de manga")
-            fila += grafo[pessoa]
+            else:
+                fila += grafo[pessoa]
+                verificadas.append(pessoa)
 
 
-    # grafo de exemplo
-    grafo = {
-        "voce": ["Bob", "Charlie"],
-        "Bob": ["David", "Eva"],
-        "Charlie": ["Frank", "Grace"],
-        "David": [],
-        "Eva": ["Isaac"],
-        "Frank": [],
-        "Grace": [],
-        "Isaac": []
-    }
+# grafo de exemplo
+grafo = {
+    "voce": ["Bob", "Charlie"],
+    "Bob": ["David", "Eva"],
+    "Charlie": ["Frank", "Grace"],
+    "David": [],
+    "Eva": ["Isaac"],
+    "Frank": [],
+    "Grace": [],
+    "Isaac": []
+}
 
-    # chama a função com um nó inicial
-    busca_mango_vendedor(grafo, "voce")
+# chama a função com um nó inicial
+busca_mango_vendedor(grafo, "voce")

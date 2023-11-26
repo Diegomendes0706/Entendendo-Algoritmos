c1ff5ac341035eef28567a4af8b2726f22f83b9e
Diegomendes0706 <diego.mendesgarcia@gmail.com>
2023-11-26 14:54:53 -0300

    cap 6 - vendedor de manga

---
+++ pesquisaemlargura1.py
@@ -0,0 +1,31 @@
+    from collections import deque
+
+
+    def pessoa_e_vendedor(nome):
+        return nome[0] == 'B' or nome[0] == 'F'
+
+
+    def busca_mango_vendedor(grafo, nome):
+        fila = deque()
+        fila += grafo[nome]
+        while fila:
+            pessoa = fila.popleft()
+            if pessoa_e_vendedor(pessoa):
+                print(pessoa + " é vendedor de manga")
+            fila += grafo[pessoa]
+
+
+    # grafo de exemplo
+    grafo = {
+        "voce": ["Bob", "Charlie"],
+        "Bob": ["David", "Eva"],
+        "Charlie": ["Frank", "Grace"],
+        "David": [],
+        "Eva": ["Isaac"],
+        "Frank": [],
+        "Grace": [],
+        "Isaac": []
+    }
+
+    # chama a função com um nó inicial
+    busca_mango_vendedor(grafo, "voce")
